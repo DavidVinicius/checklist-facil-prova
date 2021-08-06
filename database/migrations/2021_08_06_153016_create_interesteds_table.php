5b09602fc9282741593fd87a70c42fb7fd743342
David Vinicius <de.175@hotmail.com>
2021-08-06 12:39:29 -0300

    Criado migrations para tabelas de bolos e interessados. Excluído models desnecessárias

--- database/migrations/2021_08_06_153016_create_interesteds_table.php
+++ database/migrations/2021_08_06_153016_create_interesteds_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateInterestedsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('interesteds', function (Blueprint $table) {
+            $table->id();            
+            $table->integer("cake_id")->unsigned();
+            $table->foreign("cake_id")->references('id')->on('cakes')->onDelete('cascade');
+            $table->string("email");
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('interesteds');
     }
 }
